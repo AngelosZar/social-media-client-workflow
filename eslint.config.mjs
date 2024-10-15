024c99a3c7e9ff0a134dd32e1bd2fac6a3b27a48
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-10-15 17:35:52 +0200

    dev i of jest and babel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {
+        files: ['**/*.test.js'],
+        languageOptions: {
+            globals: {
+                ...globals.jest,
+            },
+        },
+        plugins: {
+            jest: jestPlugin,
+        },
+        rules: {
+            ...jestPlugin.configs.recommended.rules,
+        },
+    },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
 ];
