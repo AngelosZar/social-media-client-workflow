570a8ad8bf0aef90390caa9c2fa7b14bb6d54724
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-10-19 10:08:49 +0200

    logout test functionality

--- cypress/e2e/3-testing/logout.cy.js
+++ cypress/e2e/3-testing/logout.cy.js
@@ -6,14 +6,14 @@ describe('Logout Function', () => {
         cy.login();
         cy.wait(1000);
         // get the logout button / element/
-        // cy.window().its('localStorage.token').should('eq', 'accessToken');
-        // cy.url().should('include', 'profile');
+        cy.url().should('include', 'profile');
 
         // click on logout btn
-        // cy.get('button[data-auth="logout"]').click();
-        // cy.url().should('include', 'login');
-
-        // clear storage and check if token is removed
-        // cy.window().its('localStorage.token').should('be.undefined');
+        cy.get('button[data-auth="logout"]').click();
+        cy.wait(1000);
+        cy.window().its('localStorage.token').should('be.undefined');
+        // make sure the user is redirected to the login page
+        cy.get('#registerModal').should('be.visible');
+        cy.wait(1000);
     });
 });
