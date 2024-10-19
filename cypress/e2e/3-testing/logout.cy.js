dca0f11309967a55d249cc9b1656c6b366754a64
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-10-19 10:18:39 +0200

    created a custom function to handle the log out functionality that is now only called in the logoutest

--- cypress/e2e/3-testing/logout.cy.js
+++ cypress/e2e/3-testing/logout.cy.js
@@ -2,18 +2,9 @@ describe('Logout Function', () => {
     beforeEach(() => {
         cy.visit('./');
     });
-    it('should logout successfully and remove user data', () => {
+    it('should logout successfully and remove access token', () => {
         cy.login();
         cy.wait(1000);
-        // get the logout button / element/
-        cy.url().should('include', 'profile');
-
-        // click on logout btn
-        cy.get('button[data-auth="logout"]').click();
-        cy.wait(1000);
-        cy.window().its('localStorage.token').should('be.undefined');
-        // make sure the user is redirected to the login page
-        cy.get('#registerModal').should('be.visible');
-        cy.wait(1000);
+        cy.logout();
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -173,4 +173,15 @@ Cypress.Commands.add(
 
 //
 // Successful logout
-// Cypress.Commands.add('logout', () => {  })
+Cypress.Commands.add('logout', () => {
+    // get the logout button / element/
+    cy.url().should('include', 'profile');
+
+    // click on logout btn
+    cy.get('button[data-auth="logout"]').click();
+    cy.wait(1000);
+    cy.window().its('localStorage.token').should('be.undefined');
+    // make sure the user is redirected to the login page
+    cy.get('#registerModal').should('be.visible');
+    cy.wait(1000);
+});
